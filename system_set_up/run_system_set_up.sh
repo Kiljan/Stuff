0eb3d4b6a108fc6a425fbd1ea76a1fd1ff3fbbf8
Kiljan <vv@localhost.localdomain>
2024-10-05 19:45:14 -0400

    add new config v1

--- system_set_up/run_system_set_up.sh
+++ system_set_up/run_system_set_up.sh
@@ -10,6 +10,9 @@ unzip CascadiaMono.zip -d $FONTS
 fc-cache -f -v
 
 #Basic config upload
-cp -avr .config/i3 $HOME/.config
-cp -avr .config/polybar $HOME/.config
-cp -avr .config/nvim $HOME/.config
+cp -avr config/i3 $HOME/.config
+cp -avr config/polybar $HOME/.config
+cp -avr config/nvim $HOME/.config
+
+#For neovim
+#:MasonInstall rust-analyzer codelldb
